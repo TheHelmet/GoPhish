b99f86f2dde6fc1d23d876bd5d0c617560b77c45
TheHelmet <ncarriage@gmail.com>
2024-02-09 00:16:24 +1100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,26 @@
-# provider information
-
-variable "key_pair" {
-  type = string
+variable "admin_ip" {
+  default     = ["127.0.0.1/32", "159.196.130.62/32"]
+  description = "admin IP addresses in CIDR format"
 }
 
-# network information
+##variable "ec2_vpc_id" {
+ ## description = "ID of AWS VPC"
+  ##default     = aws_vpc.vpc_honeypot.id
+##}
 
-variable "my_ip" {
-  type = string
+
+##variable "ec2_subnet_id" {
+  ##description = "ID of AWS VPC subnet"
+  ##default     = aws_subnet.subnet_honeynet_1.id
+##}
+
+
+variable "ec2_region" {
+  description = "AWS region to launch servers"
+  default     = "ap-southeast-2"
+}
+
+
+variable "key_pair" {
+  default = "helmet-keypair"
 }

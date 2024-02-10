3442f2193ab2049f6c22983635d3732d2660b476
TheHelmet <ncarriage@gmail.com>
2024-02-10 21:25:45 +1100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "instance_public_ip" {
-  value = aws_instance.web_server.public_ip
+  value = aws_instance.Phisherman1.public_ip
   description = "Public IP of the EC2 instance"
 }
 
 output "instance_url" {
-  value = "http://${aws_instance.web_server.public_ip}"
+  value = "http://${aws_instance.Phisherman1.public_ip}"
   description = "URL to access the NGINX server on the EC2 instance"

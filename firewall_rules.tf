9f3d7146ed62618769905cb060980acec5ce5ed7
TheHelmet <ncarriage@gmail.com>
2024-02-11 12:14:48 +1100

    Update firewall_rules.tf

--- firewall_rules.tf
+++ firewall_rules.tf
@@ -18,7 +18,7 @@ resource "aws_vpc_security_group_ingress_rule" "ssh_inbound_my_ip" {
   security_group_id = aws_security_group.phish.id
   description       = "SSH Inbound from runner and my OP"
   from_port         = 22
-  cidr_ipv4         = var.admin_ip
+  cidr_blocks        = var.admin_ip
   to_port           = 22
   ip_protocol       = "tcp"
 }
@@ -26,9 +26,9 @@ resource "aws_vpc_security_group_ingress_rule" "ssh_inbound_my_ip" {
 
 resource "aws_vpc_security_group_ingress_rule" "https_admin_inbound_my_ip" {
   security_group_id = aws_security_group.phish.id
-  description       = "SSH Inbound from runner and my OP"
+  description       = "HTTPs Inbound for mgmt"
   from_port         = 3333
-  cidr_ipv4         = var.admin_ip
+  cidr_blocks      = var.admin_ip
   to_port           = 3333
   ip_protocol       = "tcp"
 }

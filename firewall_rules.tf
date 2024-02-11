d6ee0f9a7be527e30abdc26c8d9ddb0cca14a7fc
TheHelmet <ncarriage@gmail.com>
2024-02-11 12:24:01 +1100

    Update firewall_rules.tf

--- firewall_rules.tf
+++ firewall_rules.tf
@@ -6,7 +6,8 @@ resource "aws_security_group" "phish" {
 
 # GENERAL
 
-resource "aws_vpc_security_group_egress_rule" "all" {
+resource "aws_vpc_security_group_rule" "outbound_all" {
+  type = "egress"
   security_group_id = aws_security_group.phish.id
   description       = "all outbound"
   cidr_ipv4         = "0.0.0.0/0"
@@ -14,21 +15,23 @@ resource "aws_vpc_security_group_egress_rule" "all" {
 }
 
 
-resource "aws_vpc_security_group_ingress_rule" "ssh_inbound_my_ip" {
+resource "aws_vpc_security_group_rule" "ssh_inbound_my_ip" {
+  type = "ingress"
   security_group_id = aws_security_group.phish.id
   description       = "SSH Inbound from runner and my OP"
   from_port         = 22
-  cidr_ipv4         = "${coalesce(var.my_ip, "192.168.0.1")}/32"
+  cidr_blocks        = [var.admin_ip]
   to_port           = 22
   ip_protocol       = "tcp"
 }
 
 
-resource "aws_vpc_security_group_ingress_rule" "https_admin_inbound_my_ip" {
+resource "aws_vpc_security_group_rule" "mgmt_inbound_my_ip" {
+  type = "ingress"
   security_group_id = aws_security_group.phish.id
-  description       = "HTTPs Inbound for mgmt"
+  description       = "SSH Inbound from runner and my OP"
   from_port         = 3333
-  cidr_ipv4         = "${coalesce(var.my_ip, "192.168.0.1")}/32"
-  to_port           = 3333
+  cidr_blocks        = [var.admin_ip]
+  to_port           = 333
   ip_protocol       = "tcp"
 }

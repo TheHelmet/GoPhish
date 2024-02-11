1454f8cf6d125a7e15d4d92a58fd872c5a2c21e0
TheHelmet <ncarriage@gmail.com>
2024-02-11 11:59:09 +1100

    Create firewall_rules.tf

---
+++ firewall_rules.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "phish" {
+  name        = "Phisherman1"
+  description = "GoPhish"
+  vpc_id      = aws_vpc.vpc_phishnet.id
+}
+
+# GENERAL
+
+resource "aws_vpc_security_group_egress_rule" "all" {
+  security_group_id = aws_security_group.phish.id
+  description       = "all outbound"
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = -1
+}
+
+
+resource "aws_vpc_security_group_ingress_rule" "ssh_inbound_my_ip" {
+  security_group_id = aws_security_group.phish.id
+  description       = "SSH Inbound from runner and my OP"
+  from_port         = 22
+  cidr_ipv4         = var.admin_ip
+  to_port           = 22
+  ip_protocol       = "tcp"
+}
+
+
+resource "aws_vpc_security_group_ingress_rule" "https_admin_inbound_my_ip" {
+  security_group_id = aws_security_group.phish.id
+  description       = "SSH Inbound from runner and my OP"
+  from_port         = 3333
+  cidr_ipv4         = var.admin_ip
+  to_port           = 3333
+  ip_protocol       = "tcp"
+}
